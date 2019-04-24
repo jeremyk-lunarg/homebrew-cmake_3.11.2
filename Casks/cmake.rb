c4544fdfe793e7e859cd274334205a99509520ed
Jeremy Kniager <jeremyk@lunarg.com>
2019-04-24 15:49:01 -0600

    build: Create formula

---
+++ Casks/cmake.rb
@@ -0,0 +1,18 @@
+cask 'cmake' do
+  version '3.11.2'
+  sha256 '8698f68bd62647993c3371571a1d326b4ee33947301cfec4592a7908c737ac36'
+
+  url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
+  name 'CMake'
+  homepage 'https://cmake.org/'
+
+  conflicts_with formula: 'cmake'
+
+  app 'CMake.app'
+  binary "#{appdir}/CMake.app/Contents/bin/cmake"
+  binary "#{appdir}/CMake.app/Contents/bin/ccmake"
+  binary "#{appdir}/CMake.app/Contents/bin/cmakexbuild"
+  binary "#{appdir}/CMake.app/Contents/bin/cpack"
+  binary "#{appdir}/CMake.app/Contents/bin/ctest"
+  binary "#{appdir}/CMake.app/Contents/bin/cmake-gui"
+end
